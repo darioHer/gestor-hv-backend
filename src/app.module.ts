a41c99fe51a812b445d909e4237f2f31e3a14162
HaroldM126 <menesesharoldyesid@gmail.com>
2025-09-16 17:09:31 -0500

    feat(convocatoria): cierre automático con cron y endpoint manual

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/orm.config';
 import { DocenteModule } from './module/docentes/docente.module';
 import { ConvocatoriaModule } from './module/convocatorias/convocatoria.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 
 @Module({
@@ -13,6 +14,7 @@ import { ConvocatoriaModule } from './module/convocatorias/convocatoria.module';
       inject: [ConfigService],
       useFactory: typeOrmConfig,
     }),
+    ScheduleModule.forRoot(),
     DocenteModule,
     ConvocatoriaModule,
   
